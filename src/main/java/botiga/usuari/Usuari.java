69487f41e316e9a819cdd277c5d9d33e95356d71
acuenca <acuenca@institutjaumehuguet.cat>
2025-04-29 20:23:14 +0200

    Usuari Arnau fet !

--- src/main/java/botiga/usuari/Usuari.java
+++ src/main/java/botiga/usuari/Usuari.java
@@ -1,7 +1,31 @@
 package main.java.botiga.usuari;
 
 public class Usuari {
-    // La classe Usuari modela cada persona que utilitza el sistema. Inclou camps com el nom,
-    // el correu electrònic i el rol (administrador o client), entre d’altres possibles dades.
-    // Això ens permetria, per exemple, restringir l’accés a determinades funcionalitats (com afegir productes) només a administradors.
+    private String nom;
+    private String correuElectronic;
+    private String rol; // "administrador" o "client"
+
+    public Usuari(String nom, String correuElectronic, String rol) {
+        this.nom = nom;
+        this.correuElectronic = correuElectronic;
+        this.rol = rol;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getCorreuElectronic() {
+        return correuElectronic;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public boolean esAdministrador() {
+        return "administrador".equalsIgnoreCase(rol);
+    }
+    //hola
 }
+
