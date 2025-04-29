36bfbed4b363916f989931bad9a690ee544bb156
acuenca <acuenca@institutjaumehuguet.cat>
2025-04-29 20:56:43 +0200

    Usuari Arnau fet i completat !

--- src/main/java/botiga/usuari/Usuari.java
+++ src/main/java/botiga/usuari/Usuari.java
@@ -1,10 +1,11 @@
 package main.java.botiga.usuari;
 
 public class Usuari {
+    // crem els 3 atributs
     private String nom;
     private String correuElectronic;
     private String rol; // "administrador" o "client"
-
+    // Generem get/set i construcutor
     public Usuari(String nom, String correuElectronic, String rol) {
         this.nom = nom;
         this.correuElectronic = correuElectronic;
