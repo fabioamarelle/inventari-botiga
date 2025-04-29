c1e89ffabc7e7bfe327e777bbb4ed2f216ecedee
burdi1 <burdi1@institutjaumehuguet.cat>
2025-04-29 20:46:09 +0200

    Primera pujada

--- src/main/java/botiga/utilitats/InputHelper.java
+++ src/main/java/botiga/utilitats/InputHelper.java
@@ -1,10 +1,59 @@
 package main.java.botiga.utilitats;
 
+import java.util.Scanner;
+
 public class InputHelper {
-    // La classe InputHelper es dissenya per facilitar la lectura de dades de l'usuari des de la consola.
-    // Aquest tipus de classe és molt útil quan tens diverses parts del codi on necessites llegir dades del teclat.
-    // En lloc de repetir el mateix codi cada vegada que has de fer una lectura, la classe InputHelper centralitza
-    // la lògica de validació de les entrades de l'usuari i la fa reutilitzable.  Aquesta classe també ajuda a evitar
-    // errors d'entrada com quan l'usuari escriu caràcters en lloc de nombres, o quan introdueix valors que no compleixen
-    // amb el format esperat (per exemple, un número negatiu quan s'espera un valor positiu).
+    private static Scanner scanner = new Scanner(System.in);
+    public static int llegirEnter() {
+        Scanner scanner = new Scanner(System.in);
+        int num = 0;
+        boolean valid = false;
+        while (!valid) {
+            try {
+                System.out.print("Introdueix un número enter: ");
+                num = Integer.parseInt(scanner.nextLine());
+                valid = true;
+            } catch (NumberFormatException e) {
+                System.out.println("Error: No és un número vàlid. Torna a intentar-ho.");
+            }
+        }
+        return num;
+    }
+    public static int llegirEnterPositiu() {
+        int num;
+        do {
+            num = llegirEnter();
+            if (num <= 0) {
+                System.out.println("Introdueix un número positiu.");
+            }
+        } while (num <= 0);
+        return num;
+    }
+    public static int llegirDecimal() {
+        double num = 0.0;
+        boolean valid = false;
+        while (!valid) {
+            try{
+                System.out.println("Introdueix un numero decimal.");
+                num = Double.parseDouble(scanner.nextLine());
+                valid = true;
+            }catch (NumberFormatException e){
+                System.out.println("Introdueix un numero decimal, el que has posat no es valid.");
+            }
+        }
+        return (int) num;
+    }
+    public static String Stringbuit(){
+        String input = "";
+        do
+        {
+            System.out.println("Introdueix un text");
+            input = scanner.nextLine();
+            if (input.isEmpty()){
+                System.out.println("El text estava buit, introdueix un de valid ");
+        }
+        }
+        while (input.isEmpty());
+        return input;
+    }
 }
