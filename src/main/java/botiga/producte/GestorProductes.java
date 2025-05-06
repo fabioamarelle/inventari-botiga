c8fb7773b846dd8ecf140b83cd379c625660165a
Cualex <acuenca@institutjaumehuguet.cat>
2025-05-06 19:49:34 +0200

    GestorProductes temporal

--- src/main/java/botiga/producte/GestorProductes.java
+++ src/main/java/botiga/producte/GestorProductes.java
@@ -1,8 +1,69 @@
 package main.java.botiga.producte;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class GestorProductes {
-    // La classe GestorProductes és responsable de totes les operacions relacionades amb la gestió del catàleg:
-    // afegir nous productes, cercar-ne per nom (amb opcionalitat), eliminar-los, aplicar descomptes, etc.
-    // Aquesta classe actua com a capa de servei interna que encapsula les regles de negoci relacionades amb els productes.
-    // El seu ús permet centralitzar la lògica d'accés i manipulació dels objectes Producte i garantir consistència a tota l’aplicació.
+    //llista per gestionar productes
+    private ArrayList<Producte> cataleg;
+    public GestorProductes() {
+        this.cataleg = new ArrayList<>();
+    }
+
+    //afegir productes
+    public void afegirProducte(Producte p) {
+        cataleg.add(p);
+    }
+
+
+    //cerca de producte per nom
+    public ArrayList<Producte> cercarPerNom(String nomParcial) {
+        ArrayList<Producte> resultats = new ArrayList<>();
+
+        for (Producte p : cataleg) {
+
+            String nomDelProducte = p.getNom();
+            String nomProducteMinuscules = nomDelProducte.toLowerCase();
+            String nomParcialMinuscules = nomParcial.toLowerCase();
+
+            if (nomProducteMinuscules.contains(nomParcialMinuscules)) {
+                resultats.add(p);
+            }
+        }
+        return resultats;
+    }
+
+    //eliminar producte per nom
+    public boolean eliminarProducte(String nom) {
+        for (Producte p : cataleg) {
+            String nomProducte = p.getNom();
+            String nomProducteMinuscules = nomProducte.toLowerCase();
+            String nomABuscarMinuscules = nom.toLowerCase();
+
+            if (nomProducteMinuscules.equals(nomABuscarMinuscules)) {
+                cataleg.remove(p);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //aplicar descuento al producto
+    public void aplicarDescompte(String nom ,double percentatge) {
+        ArrayList<Producte> resultats = cercarPerNom(nom);
+        for (Producte p : resultats) {
+            double preuActual = p.getPreu();
+            double descompte = percentatge / 100.0;
+            double nouPreu = preuActual * (1 - descompte);
+            double preuArrodonit = Math.round(nouPreu * 100.0) / 100.0;
+            p.setPreu(preuArrodonit);
+        }
+    }
+
+    //mostrar el catalogo de productos
+    public void mostrarCataleg() {
+        for (Producte p : cataleg) {
+            System.out.println(p);
+        }
+    }
 }
--- src/test/java/botiga/producte/GestorProductesTest.java
+++ src/test/java/botiga/producte/GestorProductesTest.java
@@ -1,20 +1,94 @@
 package test.java.botiga.producte;
 
+import main.java.botiga.producte.Producte;
+import main.java.botiga.producte.GestorProductes;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 
 public class GestorProductesTest {
-    // Ejemplo de test
+
+    // Test para añadir un producto
     @Test
-    public void addTwoNumbersOK(){
+    public void afegirProducteFunciona() {
         // ARRANGE
-        int a = 1;
-        int b = 2;
+        GestorProductes gestor = new GestorProductes();
+        Producte p = new Producte("Pizza", 5.0,1);
 
         // ACT
-        int result = a + b;
+        gestor.afegirProducte(p);
+        List<Producte> resultats = gestor.cercarPerNom("Pizza");
 
         // ASSERT
-        assert (a + b == 3);
+        assert (resultats.size() == 1);
+    }
+
+    // Test para buscar un producto
+    @Test
+    public void cercarProducteFunciona() {
+        // ARRANGE
+        GestorProductes gestor = new GestorProductes();
+        Producte p = new Producte("Llet", 1.2,1);
+        gestor.afegirProducte(p);
+
+        // ACT
+        List<Producte> resultats = gestor.cercarPerNom("lle");
+
+        // ASSERT
+        assert (resultats.size() == 1);
+    }
+
+    // Test para eliminar un producto
+    @Test
+    public void eliminarProducteFunciona() {
+        // ARRANGE
+        GestorProductes gestor = new GestorProductes();
+        Producte p = new Producte("Pa", 0.9,1);
+        gestor.afegirProducte(p);
+
+        // ACT
+        boolean eliminat = gestor.eliminarProducte("Pa");
+
+        // ASSERT
+        assert (eliminat);
+    }
+
+    // Test para aplicar un descuento
+    @Test
+    public void aplicarDescompteFunciona() {
+        // ARRANGE
+        GestorProductes gestor = new GestorProductes();
+        Producte p = new Producte("Aigua", 1.0,1);
+        Producte p2 = new Producte("Pera", 5.0,3);
+        gestor.afegirProducte(p);
+        gestor.afegirProducte(p2);
+
+        // ACT
+        gestor.aplicarDescompte("Aigua", 10); // 10% de descuento
+
+        // ASSERT
+        assert (p.getPreu() == 0.9);
+        assert (p2.getPreu() == 5.0);
+    }
+
+    @Test
+    public void mostrarCatalegFunciona(){
+        // ARRANGE
+        GestorProductes gestor = new GestorProductes();
+        Producte p = new Producte("Aigua", 1.0,1);
+        Producte p2 = new Producte("Pera", 5.0,3);
+        gestor.afegirProducte(p);
+        gestor.afegirProducte(p2);
+        boolean error = false;
+
+        // ACT
+        try {
+            gestor.mostrarCataleg();
+        } catch (Exception ex) {
+            error = true;
+        }
+
+        // ASSERT
+        assert (error == false);
     }
 }
