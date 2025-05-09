3efa946b2dce200fdef2377250621bdd08676e3e
Fabio Amarelle Rodrigues <148346868+fabioamarelle@users.noreply.github.com>
2025-05-09 17:54:31 +0200

    Usuaris

---
+++ src/test/java/botiga/usuari/UsuariTest.java
@@ -0,0 +1,33 @@
+package test.java.botiga.usuari;
+import main.java.botiga.usuari.Rol;
+import main.java.botiga.usuari.Usuari;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+//Usuari test
+public class UsuariTest {
+
+    @Test
+    public void testConstructorIGetters() {
+        //verifiquem que els constructors , i els get and sett
+        Usuari usuari = new Usuari("Arnau", "arnau@gmail.com", Rol.CLIENT);
+
+        assertEquals("Arnau", usuari.getNom());
+        assertEquals("arnau@gmail.com", usuari.getCorreuElectronic());
+        assertEquals(Rol.CLIENT, usuari.getRol());
+    }
+
+    @Test
+    public void testEsAdministradorAmbAdministrador() {
+        // crem els Usuaris i verifquem
+        Usuari usuariAdmin = new Usuari("joel", "joel@gmail.com", Rol.ADMINISTRADOR);
+
+        assertTrue(usuariAdmin.esAdministrador());
+    }
+
+    @Test
+    public void testEsAdministradorAmbClient() {
+        Usuari usuariClient = new Usuari("edgar", "edgar@gmail.com", Rol.CLIENT);
+
+        assertFalse(usuariClient.esAdministrador());
+    }
+}
