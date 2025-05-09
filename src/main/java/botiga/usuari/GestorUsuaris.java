188b8cc459902e95945ed0b861963d1665b259dd
joliva <joliva4@institutjaumehuguet.cat>
2025-05-09 16:08:27 +0200

    GestorUsuarisTest Acabat amb Getters i setters per Joel Oliva.

--- src/main/java/botiga/usuari/GestorUsuaris.java
+++ src/main/java/botiga/usuari/GestorUsuaris.java
@@ -1,5 +1,4 @@
 package main.java.botiga.usuari;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,43 +7,50 @@ public class GestorUsuaris {
     // validar-ne l’existència, obtenir un usuari concret... Aquesta capa, igual que el GestorProductes, separa la lògica d’interacció
     // amb els usuaris del codi de la interfície o del flux principal del programa.
 
-        private List<Usuari> llistaUsuaris;
+    private List<Usuari> llistaUsuaris;
 
-        public GestorUsuaris() {
-            this.llistaUsuaris = new ArrayList<>();
-        }
+    public GestorUsuaris() {
+        this.llistaUsuaris = new ArrayList<>();
+    }
 
-        // Aqui vaig afegint els nous usuaris si no exixteixen
-        public boolean afegirUsuari(Usuari usuari) {
-            if (existeixUsuari(usuari.getCorreuElectronic())) {
-                return false;
-            }
-            llistaUsuaris.add(usuari);
-            return true;
-        }
-
-        // Comprova si existeix un usuari amb el correu electrònic donat
-        public boolean existeixUsuari(String correuElectronic) {
-            for (Usuari u : llistaUsuaris) {
-                if (u.getCorreuElectronic().equalsIgnoreCase(correuElectronic)) {
-                    return true;
-                }
-            }
+    public boolean afegirUsuari(Usuari usuari) {
+        if (existeixUsuari(usuari.getCorreuElectronic())) {
             return false;
         }
-
-        // Retorna l'usuari amb el correu electrònic donat, o null si no existeix
-        public Usuari obtenirUsuari(String correuElectronic) {
-            for (Usuari u : llistaUsuaris) {
-                if (u.getCorreuElectronic().equalsIgnoreCase(correuElectronic)) {
-                    return u;
-                }
-            }
-            return null;
-        }
-
-        // Retorna la llista completa d'usuaris
-        public List<Usuari> getTotsUsuaris() {
-            return new ArrayList<>(llistaUsuaris);
-        }
+        llistaUsuaris.add(usuari);
+        return true;
     }
+
+    public boolean existeixUsuari(String correuElectronic) {
+        for (Usuari u : llistaUsuaris) {
+            if (u.getCorreuElectronic().equalsIgnoreCase(correuElectronic)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Usuari obtenirUsuari(String correuElectronic) {
+        for (Usuari u : llistaUsuaris) {
+            if (u.getCorreuElectronic().equalsIgnoreCase(correuElectronic)) {
+                return u;
+            }
+        }
+        return null;
+    }
+
+    public List<Usuari> getTotsUsuaris() {
+        return new ArrayList<>(llistaUsuaris);
+    }
+
+    public List<Usuari> getLlistaUsuaris() {
+        return llistaUsuaris;
+    }
+
+    public void setLlistaUsuaris(List<Usuari> llistaUsuaris) {
+        this.llistaUsuaris = llistaUsuaris;
+
+    }
+
+}
+
