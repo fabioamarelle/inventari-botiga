4304ffa5930af2d5f259941dc6099f0aef68eaa6
Fabio Amarelle Rodrigues <148346868+fabioamarelle@users.noreply.github.com>
2025-05-12 20:12:12 +0200

    Millores a InputHelper.java

--- src/main/java/botiga/utilitats/InputHelper.java
+++ src/main/java/botiga/utilitats/InputHelper.java
@@ -3,8 +3,9 @@ package main.java.botiga.utilitats;
 import java.util.Scanner;
 
 public class InputHelper {
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static int llegirEnter(String missatge) {
-        Scanner scanner = new Scanner(System.in);
         int num = 0;
         boolean valid = false;
         while (!valid) {
@@ -16,11 +17,9 @@ public class InputHelper {
                 System.out.println("Error: No és un número vàlid. Torna a intentar-ho.");
             }
         }
-        scanner.close();
         return num;
     }
     public static int llegirEnterPositiu(String missatge) {
-        Scanner scanner = new Scanner(System.in);
         int num = 0;
         boolean valid = false;
         while (!valid) {
@@ -34,59 +33,56 @@ public class InputHelper {
                 System.out.println("Error: No és un número vàlid. Torna a intentar-ho.");
             }
         }
-        scanner.close();
         return num;
     }
     public static double llegirDecimal(String missatge) {
-        Scanner scanner = new Scanner(System.in);
         double num = 0.0;
         boolean valid = false;
         while (!valid) {
             try{
-                System.out.println(missatge);
+                System.out.print(missatge);
                 num = Double.parseDouble(scanner.nextLine());
                 valid = true;
             } catch (NumberFormatException e){
                 System.out.println("Introdueix un número decimal, el que has posat no és vàlid.");
             }
         }
-        scanner.close();
         return num;
     }
     public static String llegirString(String missatge){
-        Scanner scanner = new Scanner(System.in);
         String input;
         do
         {
-            System.out.println(missatge);
+            System.out.print(missatge);
             input = scanner.nextLine();
             if (input.isEmpty()){
                 System.out.println("El text estava buit, introdueix un de vàlid.");
             }
         }
         while (input.isEmpty());
-        scanner.close();
         return input;
     }
 
     public static boolean llegirBoolean(String missatge, String opcioTrue, String opcioFalse) {
-        Scanner scanner = new Scanner(System.in);
         String input;
 
         while (true) {
-            System.out.print(missatge);
+            System.out.print(missatge + "(" + opcioTrue + " / " + opcioFalse + "): ");
             input = scanner.nextLine().trim().toLowerCase();
 
-            if (input.equals(opcioTrue)) {
-                scanner.close();
+            if (input.equals(opcioTrue.trim().toLowerCase())) {
                 return true;
-            } else if (input.equals(opcioFalse)) {
-                scanner.close();
+            } else if (input.equals(opcioFalse.trim().toLowerCase())) {
                 return false;
             } else {
                 System.out.println("Error: Resposta no reconeguda. (" + opcioTrue + " / " + opcioFalse + ")");
             }
 
         }
+    }
+
+    public static void enterPerContinuar(){
+        System.out.print("Prèm Enter per a continuar...");
+        scanner.nextLine();
     }
 }
