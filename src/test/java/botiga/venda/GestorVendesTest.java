53dc3a35fe55e0c2259cc225f42afc68551ec723
yaktaou <yassaktalarej@gmail.com>
2025-05-12 19:47:02 +0200

    Venda acabat

--- src/test/java/botiga/venda/GestorVendesTest.java
+++ src/test/java/botiga/venda/GestorVendesTest.java
@@ -9,8 +9,6 @@ import main.java.botiga.venda.Transaccio;
 import main.java.botiga.venda.Venda;
 import org.junit.jupiter.api.Test;
 
-import java.awt.image.FilteredImageSource;
-import java.lang.reflect.Array;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,84 +16,83 @@ import java.util.List;
 public class GestorVendesTest {
 
     @Test
-    public void testafegirVenda(){
-        GestorVendes g = new GestorVendes();
-        Usuari u = new Usuari("Yassine","yassine@gmail.com",Rol.ADMINISTRADOR);
-        Venda v = new Venda(LocalDate.of(2024,10,23),u);
+    public void testAfegirVenda() {
+        GestorVendes gestorVendes = new GestorVendes();
+        Usuari usuari = new Usuari("Yassine", "yassine@gmail.com", Rol.ADMINISTRADOR);
+        Venda venda = new Venda(LocalDate.of(2024, 10, 23), usuari);
 
-        g.afegirVenda(v);
+        gestorVendes.afegirVenda(venda);
 
-        assert g.getLlista_vendes().size() == 1;
+        assert gestorVendes.getLlista_vendes().size() == 1;
     }
+
     @Test
-    public void testvendesPeriode(){
-        GestorVendes g = new GestorVendes();
-        Usuari u = new Usuari("Yassine","yassine@gmail.com", Rol.ADMINISTRADOR);
-        Venda v = new Venda(LocalDate.of(2024,10,23),u);
-        Venda v2 = new Venda(LocalDate.of(2024,10,24),u);
-        Venda v3 = new Venda(LocalDate.of(2024,10,29),u);
+    public void testVendesPeriode() {
+        GestorVendes gestorVendes = new GestorVendes();
+        Usuari usuari = new Usuari("Yassine", "yassine@gmail.com", Rol.ADMINISTRADOR);
 
+        Venda venda1 = new Venda(LocalDate.of(2024, 10, 23), usuari);
+        Venda venda2 = new Venda(LocalDate.of(2024, 10, 24), usuari);
+        Venda venda3 = new Venda(LocalDate.of(2024, 10, 29), usuari);
 
+        gestorVendes.afegirVenda(venda1);
+        gestorVendes.afegirVenda(venda2);
+        gestorVendes.afegirVenda(venda3);
 
-        g.afegirVenda(v);
-        g.afegirVenda(v2);
-        g.afegirVenda(v3);
-
-        ArrayList<Venda> resultat = g.vendesPeriode(LocalDate.of(2024,10,23),LocalDate.of(2024,10,25));
+        List<Venda> resultat = gestorVendes.vendesPeriode(LocalDate.of(2024, 10, 23), LocalDate.of(2024, 10, 25));
 
         assert resultat.size() == 2;
-
     }
 
     @Test
-    public void testbuscarVenda(){
+    public void testBuscarVenda() {
+        GestorUsuaris gestorUsuaris = new GestorUsuaris();
+        GestorVendes gestorVendes = new GestorVendes();
+        Usuari usuari = new Usuari("Yassine", "yassine@gmail.com", Rol.ADMINISTRADOR);
 
-        GestorUsuaris gu = new GestorUsuaris();
-        GestorVendes g = new GestorVendes();
-        Usuari u = new Usuari("Yassine","yassine@gmail.com",Rol.ADMINISTRADOR);
-        Venda v = new Venda(LocalDate.of(2024,10,23),u);
-        Venda v2 = new Venda(LocalDate.of(2024,10,24),u);
-        Venda v3 = new Venda(LocalDate.of(2024,10,29),u);
+        gestorVendes.setGestorUsuaris(gestorUsuaris);
+        gestorUsuaris.afegirUsuari(usuari);
 
-        g.setGestorUsuaris(gu);
-        gu.afegirUsuari(u);
-        g.afegirVenda(v2);
-        g.afegirVenda(v3);
-        g.afegirVenda(v);
+        Venda venda1 = new Venda(LocalDate.of(2024, 10, 23), usuari);
+        Venda venda2 = new Venda(LocalDate.of(2024, 10, 24), usuari);
+        Venda venda3 = new Venda(LocalDate.of(2024, 10, 29), usuari);
 
-        ArrayList<Venda> resultat = g.buscarVenda("Yassine",LocalDate.of(2024,10,23));
+        gestorVendes.afegirVenda(venda1);
+        gestorVendes.afegirVenda(venda2);
+        gestorVendes.afegirVenda(venda3);
+
+        List<Venda> resultat = gestorVendes.buscarVenda("Yassine", LocalDate.of(2024, 10, 23));
 
         assert resultat.size() == 1;
-
-
     }
 
     @Test
     public void testVendesProducte() {
-        GestorUsuaris gu = new GestorUsuaris();
-        GestorVendes g = new GestorVendes();
-        Usuari u = new Usuari("Yassine", "yassine@gmail.com", Rol.ADMINISTRADOR);
-        g.setGestorUsuaris(gu);
-        gu.afegirUsuari(u);
+        GestorUsuaris gestorUsuaris = new GestorUsuaris();
+        GestorVendes gestorVendes = new GestorVendes();
+        Usuari usuari = new Usuari("Yassine", "yassine@gmail.com", Rol.ADMINISTRADOR);
 
-        Producte p1 = new Producte("Cafè", 2.5, 10);
-        Producte p2 = new Producte("Te", 1.5, 15);
+        gestorVendes.setGestorUsuaris(gestorUsuaris);
+        gestorUsuaris.afegirUsuari(usuari);
 
-        Transaccio t1 = new Transaccio(p1, 2,u);
-        Transaccio t2 = new Transaccio(p1, 1,u); // 1 Cafè més
-        Transaccio t3 = new Transaccio(p2, 3,u);
+        Producte producte1 = new Producte("Cafè", 2.5, 10);
+        Producte producte2 = new Producte("Te", 1.5, 15);
 
-        Venda venda1 = new Venda(LocalDate.of(2024, 10, 23), u);
-        venda1.afegirTransaccio(t1);
-        venda1.afegirTransaccio(t3);
+        Transaccio transaccio1 = new Transaccio(producte1, 2, usuari);  // 2 Cafès
+        Transaccio transaccio2 = new Transaccio(producte1, 1, usuari);  // 1 Cafè més
+        Transaccio transaccio3 = new Transaccio(producte2, 3, usuari);  // 3 Tés
 
-        Venda venda2 = new Venda(LocalDate.of(2024, 10, 24), u);
-        venda2.afegirTransaccio(t2);
+        Venda venda1 = new Venda(LocalDate.of(2024, 10, 23), usuari);
+        venda1.afegirTransaccio(transaccio1);
+        venda1.afegirTransaccio(transaccio3);
 
-        g.afegirVenda(venda1);
-        g.afegirVenda(venda2);
+        Venda venda2 = new Venda(LocalDate.of(2024, 10, 24), usuari);
+        venda2.afegirTransaccio(transaccio2);
 
-        int totalVendesCafè = g.vendesProducte(p1);
+        gestorVendes.afegirVenda(venda1);
+        gestorVendes.afegirVenda(venda2);
+
+        int totalVendesCafè = gestorVendes.vendesProducte(producte1);
 
         assert totalVendesCafè == 2;
     }
